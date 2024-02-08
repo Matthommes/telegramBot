7e8e4a7c005b72f450b80590e5a1a23220442073
Matthew Akahomen <alexislordqtest@gmail.com>
2024-02-08 01:35:30 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+// import { Telegraf } from "telegraf";
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command("start", (ctx) => {
+  const userName = ctx.from.username;
+  if (userName) {
+    ctx.reply(`Hello ${userName}  !!! Welcome to my first bot`);
+  } else {
+    ctx.reply("Hello There! Good  to have you here!");
+  }
+});
+
+bot.on("message", (ctx) => {
+  ctx.reply("You sent me: " + ctx.message.text);
+});
+bot.launch();
